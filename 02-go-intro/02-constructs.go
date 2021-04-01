00fdf6add6a4bbc9a72e6271f6c868a68909b693
Magesh Kuppan <tkmagesh77@gmail.com>
2021-04-01 14:37:17 +0530

    switch case construct

--- 02-go-intro/02-constructs.go
+++ 02-go-intro/02-constructs.go
@@ -42,4 +42,65 @@ func main() {
 		}
 	*/
 
+	//switch case
+
+	/*
+		no := 51
+		remainder := no % 2
+		switch remainder {
+		case 0:
+			fmt.Printf("%v is even \n", no)
+		case 3 - 2:
+			fmt.Printf("%v is odd\n", no)
+		}
+	*/
+	/*
+		0 - 3 = Terrible
+		4 - 5 = Mediocre
+		6 - 7 = Not bad
+		8 - 9 = Good
+		10 = Perfect
+	*/
+	/*
+		score := 11
+		switch score {
+		case 0, 1, 2, 3:
+			fmt.Println("Terrible")
+		case 4, 5:
+			fmt.Println("Mediocre")
+		case 6, 7:
+			fmt.Println("Not bad!")
+		case 8, 9:
+			fmt.Println("Good!!")
+		case 10:
+			fmt.Println("Perfect!!!")
+		default:
+			fmt.Printf("%v is out of range\n", score)
+		}
+	*/
+
+	//using 'fallthrough' construct
+	n := 4
+	switch n {
+	case 0:
+		fmt.Println("is zero")
+		fallthrough
+	case 1:
+		fmt.Println("is <= 1")
+		fallthrough
+	case 2:
+		fmt.Println("is <= 2")
+		fallthrough
+	case 3:
+		fmt.Println("is <= 3")
+		fallthrough
+	case 4:
+		fmt.Println("is <= 4")
+		fallthrough
+	case 5:
+		fmt.Println("is <= 5")
+		fallthrough
+	default:
+		fmt.Println("Try again!")
+	}
 }
