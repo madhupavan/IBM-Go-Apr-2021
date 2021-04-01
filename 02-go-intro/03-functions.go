55486182614d1fcd9c07bebdfb3c87611ca3619f
Magesh Kuppan <tkmagesh77@gmail.com>
2021-04-01 14:50:48 +0530

    functions - part - 1

---
+++ 02-go-intro/03-functions.go
@@ -0,0 +1,30 @@
+package main
+
+/*
+func add(x, y int) int {
+	return x + y
+}
+*/
+
+/*
+var add = func(x, y int) int {
+	return x + y
+}
+*/
+
+func main() {
+	/*
+		add := func(x, y int) int {
+			return x + y
+		}
+		fmt.Printf("Sum of 10 & 20 is %d\n", add(10, 20))
+	*/
+
+	/*
+		func(x, y int) {
+			result := x + y
+			fmt.Printf("result = %v\n", result)
+		}(100, 200)
+	*/
+
+}
