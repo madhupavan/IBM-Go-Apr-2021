f7b9683956b78b7e7ff0e9662ca9f1e465ef8d3b
Magesh Kuppan <tkmagesh77@gmail.com>
2021-04-02 11:17:07 +0530

    assignment

--- 02-go-intro/06-structs.go
+++ 02-go-intro/06-structs.go
@@ -25,7 +25,9 @@ type PerishableProduct struct {
 func main() {
 	//p := Product{id: 100, name: "Pen", cost: 10, units: 50, category: "Stationary"}
 	p := Product{100, "Pen", 10, 50, "Stationary"}
-	fmt.Println(p)
+	var pptr *Product = &p
+	fmt.Println(p.name)
+	fmt.Println(pptr.name)
 
 	pp := PerishableProduct{
 		Product{id: 101, name: "Grapes", cost: 60, units: 100, category: "Food"},
@@ -34,4 +36,21 @@ func main() {
 
 	fmt.Println(pp)
 	fmt.Println(pp.name)
+
+	//products
+	p1 := Product{6, "Pen", 10, 50, "Stationary"}
+	p2 := Product{5, "Ten", 30, 20, "Grocery"}
+	p3 := Product{4, "Den", 20, 10, "Grocery"}
+	p4 := Product{1, "Len", 50, 30, "Stationary"}
+	p5 := Product{7, "Zen", 40, 20, "Stationary"}
+	p6 := Product{9, "Ken", 40, 20, "Stationary"}
+
+	products := []Product{p1, p2, p3, p4, p5, p6}
 }
+
+/*
+Index(products, product) => index of product in the products
+Includes(products, product) => true/false
+Any(products, predicate) => true/false
+Filter(products, predicate) => products
+*/
