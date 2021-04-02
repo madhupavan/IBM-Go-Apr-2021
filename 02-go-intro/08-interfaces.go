9efc976d87d381328ee857d6fc95ab0cb31856e9
Magesh Kuppan <tkmagesh77@gmail.com>
2021-04-02 13:06:56 +0530

    interface composition

--- 02-go-intro/08-interfaces.go
+++ 02-go-intro/08-interfaces.go
@@ -29,26 +29,48 @@ func (c Circle) perimeter() float32 {
 	return 2 * math.Pi * c.radius
 }
 
-type Shape interface {
+type Ariable interface {
 	area() float32
+}
+
+type Perimeterable interface {
 	perimeter() float32
 }
 
-func PrintDimentions(s Shape) {
+type StandardShape interface {
+	Ariable
+	Perimeterable
+}
+
+func PrintDimentions(s StandardShape) {
 	fmt.Println("Area : ", s.area())
 	fmt.Println("Perimeter : ", s.perimeter())
 }
 
+type CustomShape struct {
+	size float32
+}
+
+func (cs CustomShape) area() float32 {
+	return cs.size * 10
+}
+
+func PrintArea(ar Ariable) {
+	fmt.Println("Area = ", ar.area())
+}
+
 func main() {
 	r := Rect{10, 20}
-	fmt.Println(r.area())
-	fmt.Println(r.perimeter())
+	/* fmt.Println(r.area())
+	fmt.Println(r.perimeter()) */
 
 	c := Circle{10}
-	fmt.Println(c.area())
-	fmt.Println((c.perimeter()))
+	/* fmt.Println(c.area())
+	fmt.Println((c.perimeter())) */
 
-	PrintDimentions(r)
-	PrintDimentions(c)
+	cs := CustomShape{5}
+	PrintArea(r)
+	PrintArea(c)
+	PrintArea(cs)
 
 }
