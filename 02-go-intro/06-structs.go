c7a3fa62549a9d6ddc85e7287918fec5bfdc9127
Magesh Kuppan <tkmagesh77@gmail.com>
2021-04-02 12:01:25 +0530

    assignment solution

--- 02-go-intro/06-structs.go
+++ 02-go-intro/06-structs.go
@@ -23,6 +23,7 @@ type PerishableProduct struct {
 }
 
 func main() {
+
 	//p := Product{id: 100, name: "Pen", cost: 10, units: 50, category: "Stationary"}
 	p := Product{100, "Pen", 10, 50, "Stationary"}
 	var pptr *Product = &p
@@ -46,6 +47,52 @@ func main() {
 	p6 := Product{9, "Ken", 40, 20, "Stationary"}
 
 	products := []Product{p1, p2, p3, p4, p5, p6}
+
+	fmt.Println(Index(products, p3))
+	fmt.Println("Any stationary products ? :", Any(products, func(p Product) bool {
+		return p.category == "Stationary"
+	}))
+}
+
+func Index(products []Product, p Product) int {
+	for idx, product := range products {
+		if product == p {
+			return idx
+		}
+	}
+	return -1
+}
+
+func Includes(products []Product, p Product) bool {
+	return Index(products, p) >= 0
+}
+
+func Any(products []Product, predicate func(p Product) bool) bool {
+	for _, product := range products {
+		if predicate(product) {
+			return true
+		}
+	}
+	return false
+}
+
+func All(products []Product, predicate func(p Product) bool) bool {
+	for _, product := range products {
+		if !predicate(product) {
+			return false
+		}
+	}
+	return true
+}
+
+func Filter(products []Product, predicate func(p Product) bool) []Product {
+	result := make([]Product, 0)
+	for _, product := range products {
+		if predicate(product) {
+			result = append(result, product)
+		}
+	}
+	return result
 }
 
 /*
