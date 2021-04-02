cdf867874a281fc7681c431a38d7fb7b4ca8dba1
Magesh Kuppan <tkmagesh77@gmail.com>
2021-04-02 14:20:40 +0530

    interfaces for sorting

--- 02-go-intro/08-interfaces.go
+++ 02-go-intro/08-interfaces.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"math"
+	"sort"
 )
 
 type Rect struct {
@@ -59,6 +60,39 @@ func PrintArea(ar Ariable) {
 	fmt.Println("Area = ", ar.area())
 }
 
+type Products []Product
+
+func (products Products) Print() {
+	for _, product := range products {
+		product.Print()
+	}
+}
+
+//Implementation of the "Interface" interface in "Products" for sorting
+func (products Products) Len() int {
+	return len(products)
+}
+
+func (products Products) Less(i, j int) bool {
+	return products[i].id < products[j].id
+}
+
+func (products Products) Swap(i, j int) {
+	products[i], products[j] = products[j], products[i]
+}
+
+type Product struct {
+	id       int
+	name     string
+	cost     float32
+	units    int
+	category string
+}
+
+func (p *Product) Print() {
+	fmt.Printf("Id = %d, Name = %s, Cost = %v, Units = %v, category = %s\n", p.id, p.name, p.cost, p.units, p.category)
+}
+
 func main() {
 	r := Rect{10, 20}
 	/* fmt.Println(r.area())
@@ -72,5 +106,22 @@ func main() {
 	PrintArea(r)
 	PrintArea(c)
 	PrintArea(cs)
+
+	p1 := Product{6, "Pen", 10, 50, "Stationary"}
+	p2 := Product{5, "Ten", 30, 20, "Grocery"}
+	p3 := Product{4, "Den", 20, 10, "Grocery"}
+	p4 := Product{1, "Len", 50, 30, "Stationary"}
+	p5 := Product{7, "Zen", 40, 20, "Stationary"}
+	p6 := Product{9, "Ken", 40, 20, "Stationary"}
+
+	products := Products{p1, p2, p3, p4, p5, p6}
+
+	fmt.Println("Before sorting")
+	products.Print()
+
+	sort.Sort(products)
+
+	fmt.Println("After Sorting")
+	products.Print()
 
 }
