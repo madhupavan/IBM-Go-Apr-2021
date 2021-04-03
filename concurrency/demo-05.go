35f3348eee68979a01aeb9074c2c2e07f1143801
Magesh Kuppan <tkmagesh77@gmail.com>
2021-04-03 10:25:54 +0530

    divide & conquer - solution

--- concurrency/demo-05.go
+++ concurrency/demo-05.go
@@ -2,16 +2,23 @@ package main
 
 import "fmt"
 
-func sum(nos []int) int {
+func sum(nos []int, ch chan int) {
 	result := 0
 	for _, v := range nos {
 		result += v
 	}
-	return result
+	ch <- result
 }
 
 func main() {
+	r1 := make(chan int)
+	r2 := make(chan int)
+
 	data := []int{8, 2, 5, 3, 7, 1, 9, 6, 11, 14, 17, 57, 14}
-	result := sum(data)
-	fmt.Println(result)
+	firstSet := data[:len(data)/2]
+	secondSet := data[(len(data) / 2):]
+	go sum(firstSet, r1)
+	go sum(secondSet, r2)
+	finalResult := <-r1 + <-r2
+	fmt.Println(finalResult)
 }
